8786f2f020406840c60316c8573c8d5b15fe9bf0
Matt Villa <matthewvilla805@gmail.com>
2024-02-14 00:31:51 -0800

    Add queries for Platforms, Orders, and Customers pages

--- queries.sql
+++ queries.sql
@@ -92,3 +92,127 @@ WHERE gameID = :idOfGameAndGenreToDelete;
 --Delete a genre 
 DELETE FROM Genres
 WHERE genreID = :idOfGenreToDelete;
+/* 
+ Platforms page queries
+ */
+--Get all the platforms
+SELECT *
+FROM Platforms
+ORDER BY name;
+--
+--Get all the game-platform relationships
+SELECT PlatformHasGames.platformHasGamesID,
+    Games.title as Game,
+    Platforms.name as Platform
+FROM PlatformHasGames
+    JOIN Games ON Games.gameID = PlatformHasGames.gameID
+    JOIN Platforms ON Platforms.platformID = PlatformHasGames.platformID
+ORDER BY Games.title;
+--
+--Add a new Platform
+INSERT INTO Platforms (name)
+VALUES (:platformName);
+--
+--Add a new game-platform relationship
+INSERT INTO PlatformHasGames (gameID, platformID)
+VALUES (:idOfGameToAddPlatform, :idOfPlatformToAddToGame);
+--
+--update a Platform
+UPDATE Platforms
+SET platformID = :idOfPlatformToUpdate,
+    name = :newName
+WHERE platformID = :idOfPlatformToUpdate;
+--
+--update a game-Platform relationship
+UPDATE PlatformHasGames
+SET platformHasGamesID = :idOfPlatformHasGameToUpdate,
+    gameID = :newGameID,
+    platformID = :newPlatformID
+WHERE platformHasGamesID = :idOfPlatformHasGameToUpdate;
+/* 
+ Orders page queries
+ */
+--
+--Get all the orders
+SELECT *
+FROM Orders
+ORDER BY orderID;
+--
+--Get all the game-order relationships
+SELECT OrderHasGames.orderHasGamesID,
+    Games.title AS Game,
+    Orders.date AS `Order Date`
+FROM OrderHasGames
+    JOIN Games ON Games.gameID = OrderHasGames.gameID
+    JOIN Orders ON Orders.orderID = OrderHasGames.orderID
+ORDER BY Games.title --
+    --Add a new game-order relationship
+INSERT INTO OrderHasGames (orderHasGamesID, gameID, orderID)
+VALUES (
+        :idOfGameOrder,
+        :idOfGameOnOrder,
+        :idOfOrderWithGame
+    );
+--
+--update a game-order relationship
+UPDATE OrderHasGames
+SET orderHasGamesID = :idOfGameOrder,
+    gameID = :newGameID,
+    platformID = :newPlatformID
+WHERE platformHasGamesID = :idOfPlatformHasGameToUpdate;
+--
+--Update an order
+UPDATE Orders
+SET orderID = :idOfOrderToUpdate,
+    date = :newDate,
+    customerID = :idOfCustomerToUpdate
+WHERE orderID = :idOfOrderToUpdate;
+/* 
+ Customers page queries
+ */
+--get all of the customers
+SELECT customerID AS `Customer ID`,
+    firstName AS `First Name`,
+    lastName AS `Last Name`,
+    email AS `Email`,
+    rewardPoints AS `Rewards Points`,
+    CASE
+        WHEN activeCustomer = 1 THEN 'Yes'
+        WHEN activeCustomer = 0 THEN 'No'
+    END AS `Active Customer`
+FROM Customers;
+--
+--Get all the orders
+Select orderID,
+    date AS `Date`,
+    customerID AS `Customer ID`
+FROM Orders;
+--
+--Add a new customer
+INSERT INTO Customers (
+        firstName,
+        lastName,
+        email
+    )
+VALUES (
+        :firstName,
+        :lastName,
+        :email,
+    );
+--
+--Add a new order
+INSERT INTO Orders (date, customerID)
+VALUES (
+        :date,
+        :idOfCustomerForOrder
+    );
+--
+--Update a customer
+UPDATE Customers
+SET customerID = :idOfCustomerToUpdate,
+    firstName = :newFirstName,
+    lastName = :newLastName,
+    email = :newEmail,
+    rewardPoints = :newRewardPoints,
+    activeCustomer = :newActiveCustomerStatus
+WHERE customerID = :idOfCustomerToUpdate;
