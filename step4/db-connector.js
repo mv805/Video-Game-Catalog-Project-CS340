42f888efa91c71376413e0ea92fd9aafb89bfb39
Matt Villa <matthewvilla805@gmail.com>
2024-02-19 21:40:42 -0800

    initial commit of step 4 folder

---
+++ step4/db-connector.js
@@ -0,0 +1,14 @@
+// Get an instance of mysql we can use in the app
+var mysql = require("mysql");
+
+// Create a 'connection pool' using the provided credentials
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+// Export it for use in our application
+module.exports.pool = pool;
