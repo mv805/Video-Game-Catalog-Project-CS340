aa2af22af1d75141a6049a317e36e8f3847f4545
Matt Villa <matthewvilla805@gmail.com>
2024-03-02 14:30:38 -0800

    starting on step 5 game functionality

--- step5/controllers/gameController.js
+++ step5/controllers/gameController.js
@@ -1,44 +1,156 @@
 const Game = require("../models/Game");
+const Developer = require("../models/Developer");
+const Franchise = require("../models/Franchise");
 const handleError = require("../errorHandler");
 
 const GameController = {
-  index: (req, res) => {
-    Game.getAll((err, results) => {
-      if (err) {
-        return handleError(res, err.message);
+  index: async (req, res) => {
+    try {
+      //get the last inserted and deleted parameter if it exists
+      const { lastInsertId, lastDeletedTitle, updateFormId, lastUpdateId } =
+        req.query;
+
+      //get and display all the games on first load
+      const games = await new Promise((resolve, reject) => {
+        Game.getAll((err, results) => {
+          if (err) reject(err);
+          else resolve(results);
+        });
+      });
+
+      //get franchises for the id selectors
+      const franchises = await new Promise((resolve, reject) => {
+        Franchise.getAll((err, results) => {
+          if (err) reject(err);
+          else resolve(results);
+        });
+      });
+
+      //get developers for the id selectors
+      const developers = await new Promise((resolve, reject) => {
+        Developer.getAll((err, results) => {
+          if (err) reject(err);
+          else resolve(results);
+        });
+      });
+
+      //depending on the parameters in the url, the following values might be given to the options for the next render. They are used to conditionally render parts of the html
+
+      let lastGameCreated;
+      if (lastInsertId) {
+        lastGameCreated = await new Promise((resolve, reject) => {
+          Game.findById(lastInsertId, (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+          });
+        });
       }
-      res.render("game", { games: results });
-    });
+
+      let lastGameDeleted;
+      if (lastDeletedTitle) {
+        lastGameDeleted = lastDeletedTitle;
+      }
+
+      let lastGameUpdated;
+      if (lastUpdateId) {
+        lastGameUpdated = await new Promise((resolve, reject) => {
+          Game.findById(lastUpdateId, (err, result) => {
+            if (err) reject(err);
+            else resolve(result);
+          });
+        });
+      }
+
+      let updateFormFillData;
+      if (updateFormId) {
+        updateFormFillData = await new Promise((resolve, reject) => {
+          Game.findById(updateFormId, (err, result) => {
+            if (err) reject(err);
+            else resolve(result[0]);
+          });
+        });
+      }
+
+      //template render options
+      let renderOptions = {
+        games,
+        franchises,
+        developers,
+        lastGameCreated,
+        lastGameDeleted,
+        updateFormFillData,
+        lastGameUpdated,
+      };
+
+      res.render("game", renderOptions);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("An error occured rendering the game page.");
+    }
   },
-  find: (req, res) => {
-    const { title, developer, franchise, platform } = req.query;
+  add: (req, res) => {
+    //The paramaters for creating a new game. Title, release year are required
+    const { title, releaseYear, price, developerId, franchiseId } = req.body;
 
-    let termToFind;
-    let searchInputTerm;
-
-    if (title) {
-      termToFind = "title";
-      searchInputTerm = title;
-    } else if (developer) {
-      termToFind = "developer";
-      searchInputTerm = developer;
-    } else if (franchise) {
-      termToFind = "franchise";
-      searchInputTerm = franchise;
-    } else if (platform) {
-      termToFind = "platform";
-      searchInputTerm = platform;
-    } else {
-      res.status(400).send("No search term provided");
-      return;
+    if (!title || !releaseYear) {
+      return handleError(
+        res,
+        "Game Title and Release Year is required when creating a Game."
+      );
+    } else if (isNaN(Number(releaseYear))) {
+      return handleError(res, "Release year must be a number.");
     }
 
-    Game.find(termToFind, searchInputTerm, (err, results) => {
-      if (err) {
-        return handleError(res, err.message);
+    Game.add(
+      title,
+      releaseYear,
+      price,
+      developerId,
+      franchiseId,
+      (err, results) => {
+        if (err) {
+          console.log(err);
+          // Redirect to an error page
+          return handleError(res, err.message);
+        }
+        //redirect to index, but give the last created row id so it can be highlighted to the user
+        res.redirect(`/?lastInsertId=${results.insertId}`);
       }
-      res.redirect("/");
-    });
+    );
+  },
+  delete: async (req, res) => {
+    try {
+      const { gameId } = req.body;
+
+      if (!gameId) {
+        return handleError(
+          res,
+          "The Game ID is required to delete a row."
+        );
+      }
+
+      const lastGameDeleted = await new Promise((resolve, reject) => {
+        Game.findById(gameId, (err, result) => {
+          console.log(result);
+          if (err) reject(err);
+          else resolve(result[0]);
+        });
+      });
+
+      await new Promise((resolve, reject) => {
+        Game.delete(gameId, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+
+      res.redirect(`/?lastDeletedTitle=${lastGameDeleted["Title"]}`);
+    } catch (err) {
+      return handleError(res, err.message);
+    }
   },
 };
 
--- step5/models/Game.js
+++ step5/models/Game.js
@@ -4,20 +4,86 @@ const Game = {
   getAll: (callback) => {
     db.pool.query(
       `
-    SELECT Games.gameID AS 'Game ID',
-        Games.title AS 'Title',
-        Games.releaseYear AS 'Release Year',
-        CONCAT('$', Games.price) AS 'Price',
-        IFNULL(Developers.name, 'N/A') as 'Developer',
-        IFNULL(Franchises.title, 'N/A') as 'Franchise'
-    FROM Games
-        LEFT JOIN Developers ON Developers.developerID = Games.developerID
-        LEFT JOIN Franchises ON Franchises.franchiseID = Games.franchiseID
-    ORDER BY Games.gameID;
-    `,
+      SELECT Games.gameID AS 'Game ID',
+          Games.title AS 'Title',
+          Games.releaseYear AS 'Release Year',
+          CONCAT('$', Games.price) AS 'Price',
+          IFNULL(Developers.name, 'N/A') as 'Developer',
+          IFNULL(Franchises.title, 'N/A') as 'Franchise'
+      FROM Games
+          LEFT JOIN Developers ON Developers.developerID = Games.developerID
+          LEFT JOIN Franchises ON Franchises.franchiseID = Games.franchiseID
+      ORDER BY Games.gameID;
+      `,
       callback
     );
   },
+  add: (title, releaseYear, price, developerId, franchiseId, callback) => {
+    let sql = `
+        INSERT INTO Games (
+            title,
+            releaseYear,
+            price,
+            developerID,
+            franchiseID
+        )
+        VALUES (
+            ?,
+            ?,
+            ${price ? "?" : "NULL"},
+            ${developerId ? "?" : "NULL"},
+            ${franchiseId ? "?" : "NULL"}
+        );
+    `;
+
+    let params = [title, releaseYear];
+    if (price) params.push(price);
+    if (developerId) params.push(developerId);
+    if (franchiseId) params.push(franchiseId);
+
+    db.pool.query(sql, params, callback);
+  },
+  findById: (gameId, callback) => {
+    db.pool.query(
+      `
+      SELECT Games.gameID AS 'Game ID',
+          Games.title AS 'Title',
+          Games.releaseYear AS 'Release Year',
+          CONCAT('$', Games.price) AS 'Price',
+          IFNULL(Developers.name, 'N/A') as 'Developer',
+          IFNULL(Franchises.title, 'N/A') as 'Franchise'
+      FROM Games
+          LEFT JOIN Developers ON Developers.developerID = Games.developerID
+          LEFT JOIN Franchises ON Franchises.franchiseID = Games.franchiseID
+      WHERE Games.gameId = ?
+      ORDER BY Games.gameID;
+      `,
+      [gameId],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, result);
+        }
+      }
+    );
+  },
+  delete: (gameId, callback) => {
+    db.pool.query(
+      `
+      DELETE FROM Games
+      WHERE gameId = ?;
+      `,
+      [gameId],
+      (err, result) => {
+        if (err) {
+          callback(err, null);
+        } else {
+          callback(null, result);
+        }
+      }
+    );
+  }
 };
 
 module.exports = Game;
